397a145b9f8ee37395b0fd2d844ef32bca730e0a
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-06-17 23:42:33 +0000

    Initial creation of project

---
+++ AR.Domain/Models/AnimalStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AR.Domain.Models
+{
+    public class AnimalStatus
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
