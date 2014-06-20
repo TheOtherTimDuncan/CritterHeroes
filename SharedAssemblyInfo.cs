9cc76f207e81eb3a078594554427df6c29f85bd3
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-06-20 19:31:47 +0000

    Changed assembly company

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Animal Rescues")]
+[assembly: AssemblyCompany("The Other Tim Duncan")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 
