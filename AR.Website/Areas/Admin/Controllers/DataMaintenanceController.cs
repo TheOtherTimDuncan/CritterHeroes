13f18a80a4a2f83242ccb2281df6edb06dfbb05f
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-06-22 23:41:16 +0000

    Added starting website

---
+++ AR.Website/Areas/Admin/Controllers/DataMaintenanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AR.Website.Areas.Admin.Controllers
+{
+    public class DataMaintenanceController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AR.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AR.Website.MvcApplication" Language="C#" %>
