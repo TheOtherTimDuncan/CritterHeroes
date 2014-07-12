ee3e7308e01ad8c01712c05dcb7c8b95a7105428
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-07-12 23:18:09 +0000

    Changed website to use a virtual directory Upgraded Bootstrap to 3.2.0 and renamed javascript file to include version Changed javascript files to use bundles with CDN fallbacks

--- AR.Website/App_Start/BundleConfig.cs
+++ AR.Website/App_Start/BundleConfig.cs
@@ -8,20 +8,46 @@ namespace AR.Website
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/Libraries/jquery-{version}.js"));
+            //BundleTable.EnableOptimizations = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/Libraries/jquery.validate*"));
+            bundles.UseCdn = true;
+
+            // jQuery
+            Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js")
+                .Include("~/Scripts/Libraries/jquery-2.1.1.*");
+            jqueryBundle.CdnFallbackExpression = "window.jQuery";
+            bundles.Add(jqueryBundle);
+
+            // jQuery validation
+            Bundle validateBundle = new ScriptBundle("~/bundles/jqueryvalidation", "//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js")
+                .Include("~/Scripts/Libraries/jquery.validate.js");
+            validateBundle.CdnFallbackExpression = "window.jQuery.validator";
+            bundles.Add(validateBundle);
+
+            // Unobtrusive validation
+            Bundle unobtrusiveBundle = new ScriptBundle("~/bundles/jqueryunobtrusive", "//ajax.aspnetcdn.com/ajax/mvc/5.1/jquery.validate.unobtrusive.min.js")
+                .Include("~/Scripts/Libraries/jquery.validate.unobtrusive.js");
+            unobtrusiveBundle.CdnFallbackExpression = "window.jQuery.validator.unobtrusive";
+            bundles.Add(unobtrusiveBundle);
+
+            // Bootstrap javascript
+            Bundle bootstrapBundle = new ScriptBundle("~/bundles/bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")
+                .Include("~/Scripts/Libraries/bootstrap-3.2.0.*");
+            bootstrapBundle.CdnFallbackExpression = "$.fn.modal";
+            bundles.Add(bootstrapBundle);
+
+            // Respond javascript
+            Bundle respondBundle = new ScriptBundle("~/bundles/respond", "//ajax.aspnetcdn.com/ajax/respond/1.2.0/respond.js")
+                .Include("~/Scripts/Libraries/respond.js");
+            respondBundle.CdnFallbackExpression = "window.respond";
+            bundles.Add(respondBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/Libraries/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/Libraries/bootstrap.js",
-                      "~/Scripts/Libraries/respond.js"));
+            Bundle modernizerBundle = new ScriptBundle("~/bundles/modernizr", "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.7.2.js")
+                .Include("~/Scripts/Libraries/modernizr-2.7.2.*");
+            modernizerBundle.CdnFallbackExpression = "window.modernizer";
+            bundles.Add(modernizerBundle);
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
