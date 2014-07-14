a7a3a94e9d0b66e6a5963803f100da55ca0791b7
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-07-14 23:00:59 +0000

    Moved cdn fallbacks to separate javascript file Added fallback for bootstrap CSS

--- AR.Website/App_Start/BundleConfig.cs
+++ AR.Website/App_Start/BundleConfig.cs
@@ -12,45 +12,43 @@ namespace AR.Website
 
             bundles.UseCdn = true;
 
+            bundles.Add(new ScriptBundle("~/bundles/cdnFallback").Include("~/Scripts/ar.cdnFallback.js"));
+
             // jQuery
             Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js")
                 .Include("~/Scripts/Libraries/jquery-2.1.1.*");
-            jqueryBundle.CdnFallbackExpression = "window.jQuery";
             bundles.Add(jqueryBundle);
 
             // jQuery validation
             Bundle validateBundle = new ScriptBundle("~/bundles/jqueryvalidation", "//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js")
                 .Include("~/Scripts/Libraries/jquery.validate.js");
-            validateBundle.CdnFallbackExpression = "window.jQuery.validator";
             bundles.Add(validateBundle);
 
             // Unobtrusive validation
             Bundle unobtrusiveBundle = new ScriptBundle("~/bundles/jqueryunobtrusive", "//ajax.aspnetcdn.com/ajax/mvc/5.1/jquery.validate.unobtrusive.min.js")
                 .Include("~/Scripts/Libraries/jquery.validate.unobtrusive.js");
-            unobtrusiveBundle.CdnFallbackExpression = "window.jQuery.validator.unobtrusive";
             bundles.Add(unobtrusiveBundle);
 
-            // Bootstrap javascript
-            Bundle bootstrapBundle = new ScriptBundle("~/bundles/bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")
+            // Bootstrap
+            Bundle boostrapScript = new ScriptBundle("~/bundles/bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")
                 .Include("~/Scripts/Libraries/bootstrap-3.2.0.*");
-            bootstrapBundle.CdnFallbackExpression = "$.fn.modal";
-            bundles.Add(bootstrapBundle);
+            Bundle bootstrapCss = new StyleBundle("~/Content/bootstrap", "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/yeti/bootstrap.min.css")
+                .Include("~/Content/bootstrap.yeti.*");
+            bundles.Add(boostrapScript);
+            bundles.Add(bootstrapCss);
 
             // Respond javascript
             Bundle respondBundle = new ScriptBundle("~/bundles/respond", "//ajax.aspnetcdn.com/ajax/respond/1.2.0/respond.js")
                 .Include("~/Scripts/Libraries/respond.js");
-            respondBundle.CdnFallbackExpression = "window.respond";
             bundles.Add(respondBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             Bundle modernizerBundle = new ScriptBundle("~/bundles/modernizr", "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.7.2.js")
                 .Include("~/Scripts/Libraries/modernizr-2.7.2.*");
-            modernizerBundle.CdnFallbackExpression = "window.modernizer";
             bundles.Add(modernizerBundle);
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
---
+++ AR.Website/Scripts/ar.cdnFallback.js
@@ -0,0 +1,58 @@
+﻿var cdnFallback = (function () {
+
+    var options = {
+        siteRoot: '/',
+        bundleRoot: 'bundles'
+    };
+
+    function writeScriptBundleUrl(bundleName) {
+        document.write('<script src="' + options.siteRoot + options.bundleRoot + '/' + bundleName + '"><\/script>');
+    }
+
+    var jqueryFallback = function (bundleName) {
+        window.jQuery || writeScriptBundleUrl(bundleName);
+    }
+
+    var jqueryValidationFallback = function (bundleName) {
+        window.jQuery.validator || writeScriptBundleUrl(bundleName);
+    }
+
+    var unobtrusiveFallback = function (bundleName) {
+        window.jQuery.validator.unobtrusive || writeScriptBundleUrl(bundleName);
+    }
+
+    var bootstrapFallback = function (bundleName) {
+        $.fn.model || writeScriptBundleUrl(bundleName);
+    }
+
+    var respondFallback = function (bundleName) {
+        window.respond || writeScriptBundleUrl(bundleName);
+    }
+
+    var modernizerFallback = function (bundleName) {
+        window.modernizer || writeScriptBundleUrl(bundleName);
+    }
+
+    var cssBootStrapFallback = function (bundlePath)
+    {
+        var checkElement = $('<div>', { id: 'bootstrap-check', class: 'hidden' }).appendTo('body');
+        if ($('#bootstrap-check').is(':visible') == true) {
+            $('<link rel="stylesheet" type="text/css" href="' + options.siteRoot + '/' + bundlePath + '">').appendTo('head');
+        }
+        checkElement.remove();
+    }
+
+    var result = {
+        options: options,
+        jqueryFallback: jqueryFallback,
+        jqueryValidationFallback: jqueryValidationFallback,
+        unobtrusiveFallback: unobtrusiveFallback,
+        bootstrapFallback: bootstrapFallback,
+        respondFallback: respondFallback,
+        modernizerFallback: modernizerFallback,
+        cssBootStrapFallback: cssBootStrapFallback
+    }
+
+    return result;
+
+})();
