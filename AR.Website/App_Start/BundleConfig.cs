2a767aa0c11adaa5b31e3c9d3486048bcd2203a8
Tim <Tim@bd96a667-a0cc-a240-8cc2-ec1d0a28dfca>
2014-07-27 17:52:03 +0000

    Moved scripts back to Scripts folder and changed solution file to add nesting Changed solution file to nest CSS files Switched site.css to site.less to use variables from variables.less

--- AR.Website/App_Start/BundleConfig.cs
+++ AR.Website/App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace AR.Website
         {
             //BundleTable.EnableOptimizations = true;
 
+            bundles.IgnoreList.Ignore("*.less");
+
             bundles.UseCdn = true;
 
             bundles.Add(new ScriptBundle("~/bundles/cdnFallback").Include("~/Scripts/ar.cdnFallback.js"));
@@ -22,22 +24,22 @@ namespace AR.Website
 
             // jQuery
             Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js")
-                .Include("~/Scripts/Libraries/jquery-2.1.1.*");
+                .Include("~/Scripts/jquery-2.1.1.*");
             bundles.Add(jqueryBundle);
 
             // jQuery validation
             Bundle validateBundle = new ScriptBundle("~/bundles/jqueryvalidation", "//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js")
-                .Include("~/Scripts/Libraries/jquery.validate.js");
+                .Include("~/Scripts/jquery.validate.js");
             bundles.Add(validateBundle);
 
             // Unobtrusive validation
             Bundle unobtrusiveBundle = new ScriptBundle("~/bundles/jqueryunobtrusive", "//ajax.aspnetcdn.com/ajax/mvc/5.1/jquery.validate.unobtrusive.min.js")
-                .Include("~/Scripts/Libraries/jquery.validate.unobtrusive.js");
+                .Include("~/Scripts/jquery.validate.unobtrusive.js");
             bundles.Add(unobtrusiveBundle);
 
             // Bootstrap
             Bundle boostrapScript = new ScriptBundle("~/bundles/bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")
-                .Include("~/Scripts/Libraries/bootstrap-3.2.0.*");
+                .Include("~/Scripts/bootstrap-3.2.0.*");
             Bundle bootstrapCss = new StyleBundle("~/Content/bootstrap", "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/yeti/bootstrap.min.css")
                 .Include("~/Content/bootstrap.yeti.*");
             bundles.Add(boostrapScript);
@@ -45,13 +47,13 @@ namespace AR.Website
 
             // Respond javascript
             Bundle respondBundle = new ScriptBundle("~/bundles/respond", "//ajax.aspnetcdn.com/ajax/respond/1.2.0/respond.js")
-                .Include("~/Scripts/Libraries/respond.js");
+                .Include("~/Scripts/respond.js");
             bundles.Add(respondBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             Bundle modernizerBundle = new ScriptBundle("~/bundles/modernizr", "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.7.2.js")
-                .Include("~/Scripts/Libraries/modernizr-2.7.2.*");
+                .Include("~/Scripts/modernizr-2.7.2.*");
             bundles.Add(modernizerBundle);
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
