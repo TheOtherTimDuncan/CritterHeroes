2d48ffe4430cec045709797ae5edd637de66caef
Tim <tduncan72@gmail.com>
2014-10-31 19:49:58 -0400

    Added configuration for Azure blob url and account name
    Added extension to create url for Azure blob
    Replaced logo images with log stored in Azure blob storage

--- AR.Website/Utility/UrlHelperExtensions.cs
+++ AR.Website/Utility/UrlHelperExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
@@ -12,6 +13,11 @@ namespace AR.Website.Utility
         public static string HomeAction(this UrlHelper urlHelper, Expression<Func<HomeController, ActionResult>> actionSelector)
         {
             return urlHelper.Action<HomeController>(actionSelector);
+        }
+
+        public static string Blob(this UrlHelper urlHelper, string filename)
+        {
+            return string.Format("{0}/{1}/{2}", ConfigurationManager.AppSettings["BlobBaseUrl"], ConfigurationManager.AppSettings["Account"], filename.ToLower());
         }
     }
 }
