26d7211e56562746af4a8c7b00da419b2191e176
Tim <tduncan72@gmail.com>
2014-11-01 16:54:37 -0400

    Installed Ninject and added beginning dependency container

---
+++ AR.Domain/Dependency/DependencyContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+
+namespace AR.Domain.Dependency
+{
+    public class DependencyContainer
+    {
+        private static IKernel _kernel;
+
+        public static T Using<T>()
+        {
+            return Kernel.Get<T>();
+        }
+
+        public static IKernel Kernel
+        {
+            get
+            {
+                if (_kernel == null)
+                {
+                    _kernel = GetStandardKernel();
+                }
+                return _kernel;
+            }
+        }
+
+        private static IKernel GetStandardKernel()
+        {
+            return new StandardKernel
+            (
+                new DependencyModule()
+            );
+        }
+    }
+}
---
+++ AR.Domain/Dependency/DependencyModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+
+namespace AR.Domain.Dependency
+{
+    public class DependencyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
