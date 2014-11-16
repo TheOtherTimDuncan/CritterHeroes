da7b55eaab3ce963a200ca0b5f4fedf49e8f185f
Tim <tduncan72@gmail.com>
2014-11-16 13:01:34 -0500

    Upgraded TOTD.Utility from 1.0.3 to 1.1.0 Added test to verify no async methods return void

--- CH.Test/BaseTest.cs
+++ CH.Test/BaseTest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using CH.Dependency;
+using FluentAssertions;
+using TOTD.Utility.EnumerableHelpers;
+using TOTD.Utility.Misc;
 
 namespace CH.Test
 {
@@ -12,6 +16,13 @@ namespace CH.Test
         public T Using<T>()
         {
             return DependencyContainer.Using<T>();
+        }
+
+        public void AssertMethodsListIsNullOrEmpty(IEnumerable<MethodInfo> methods, string assertMessage)
+        {
+            string message = "Failing methods: " + string.Join("\n", methods.Select(x => x.DeclaringType.Name + "." + x.Name));
+            Console.WriteLine(message);
+            methods.Should().BeNullOrEmpty(assertMessage);
         }
     }
 }
---
+++ CH.Test/MiscTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CH.Azure;
+using CH.Domain.Proxies;
+using CH.RescueGroups;
+using CH.Website;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Utility.Misc;
+
+namespace CH.Test
+{
+    [TestClass]
+    public class MiscTests : BaseTest
+    {
+        [TestMethod]
+        public void NoAsyncMethodsShouldReturnVoid()
+        {
+            AssertMethodsListIsNullOrEmpty(UnitTestHelper.GetAsyncVoidMethods(GetType().Assembly), "no async methods should be returning null");
+            AssertMethodsListIsNullOrEmpty(UnitTestHelper.GetAsyncVoidMethods(typeof(AzureStorage).Assembly), "no async methods should be returning null");
+            AssertMethodsListIsNullOrEmpty(UnitTestHelper.GetAsyncVoidMethods(typeof(MvcApplication).Assembly), "no async methods should be returning null");
+        }
+    }
+}
