cf0da58adf121f1d6824a7121d4d49f6befbc257
Tim <tduncan72@gmail.com>
2014-11-23 13:07:23 -0500

    Fixed query/command bindings so they work at runtime

--- CH.Dependency/Modules/QueryCommandModule.cs
+++ CH.Dependency/Modules/QueryCommandModule.cs
@@ -16,15 +16,31 @@ namespace CH.Dependency.Modules
             Bind<IQueryDispatcher>().To<QueryDispatcher>();
             Bind<ICommandDispatcher>().To<CommandDispatcher>();
 
+            // Binding with AppDomain.CurrentDomain fails at runtime so it can't be used
+
             Kernel.Bind(x => x
-                .From(AppDomain.CurrentDomain.GetAssemblies())
+                .FromAssemblyContaining(typeof(IQueryHandler<,>))
                 .SelectAllClasses()
                 .InheritedFrom(typeof(IQueryHandler<,>))
                 .BindAllInterfaces()
             );
 
             Kernel.Bind(x => x
-                .From(AppDomain.CurrentDomain.GetAssemblies())
+                .FromAssembliesMatching("CH.Website")
+                .SelectAllClasses()
+                .InheritedFrom(typeof(IQueryHandler<,>))
+                .BindAllInterfaces()
+            );
+
+            Kernel.Bind(x => x
+                .FromAssemblyContaining(typeof(IQueryHandler<,>))
+                .SelectAllClasses()
+                .InheritedFrom(typeof(ICommandHandler<>))
+                .BindAllInterfaces()
+            );
+
+            Kernel.Bind(x => x
+                .FromAssembliesMatching("CH.Website")
                 .SelectAllClasses()
                 .InheritedFrom(typeof(ICommandHandler<>))
                 .BindAllInterfaces()
