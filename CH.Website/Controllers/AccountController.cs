4a1705ec3ddd8986bf38d51f8908f1a0ad6b4ecc
Tim <tduncan72@gmail.com>
2014-11-30 14:26:02 -0500

    Removed unneeded code from AccountController

--- CH.Website/Controllers/AccountController.cs
+++ CH.Website/Controllers/AccountController.cs
@@ -1,32 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using CH.Domain.Commands;
 using CH.Domain.Contracts.Commands;
-using CH.Domain.Contracts.Identity;
-using CH.Domain.Contracts.Logging;
 using CH.Domain.Contracts.Queries;
-using CH.Domain.Identity;
-using CH.Domain.Models.Logging;
 using CH.Website.Models;
 using CH.Website.Services.Queries;
-using CH.Website.Utility;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 
 namespace CH.Website.Controllers
 {
     public class AccountController : BaseController
     {
-        private ApplicationUserManager _userManager;
         private IAuthenticationManager _authenticationManager;
-        private IUserLogger _userLogger;
 
         private IQueryDispatcher _queryDispatcher;
         private ICommandDispatcher _commandDispatcher;
@@ -35,27 +24,6 @@ namespace CH.Website.Controllers
         {
             this._queryDispatcher = queryDispatcher;
             this._commandDispatcher = commandDispatcher;
-
-            _userLogger = Using<IUserLogger>();
-        }
-
-        public AccountController(ApplicationUserManager userManager, IAuthenticationManager authenticationManager, IUserLogger userLogger)
-        {
-            _userManager = userManager;
-            _authenticationManager = authenticationManager;
-            _userLogger = userLogger;
-        }
-
-        public ApplicationUserManager UserManager
-        {
-            get
-            {
-                if (_userManager == null)
-                {
-                    _userManager = Using<ApplicationUserManager>();
-                }
-                return _userManager;
-            }
         }
 
         public IAuthenticationManager AuthenticationManager
