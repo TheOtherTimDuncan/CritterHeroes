3015f6017c958180a5148df1e6338538d68b82e5
Tim <tduncan72@gmail.com>
2014-12-03 21:05:16 -0500

    Added missed files

---
+++ CH.Domain/Queries/UserQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CH.Domain.Contracts.Identity;
+using CH.Domain.Contracts.Queries;
+using CH.Domain.Identity;
+using TOTD.Utility.ExceptionHelpers;
+
+namespace CH.Domain.Queries
+{
+    public class UserQueryHandler : IQueryHandler<UserQuery, IdentityUser>
+    {
+        private IApplicationUserStore _userStore;
+
+        public UserQueryHandler(IApplicationUserStore userStore)
+        {
+            this._userStore = userStore;
+        }
+
+        public async Task<IdentityUser> Retrieve(UserQuery query)
+        {
+            ThrowIf.Argument.IsNull(query, "query");
+            return await _userStore.FindByNameAsync(query.Username);
+        }
+    }
+}
---
+++ CH.Test/QueryTests/UserQueryHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CH.Domain.Contracts.Identity;
+using CH.Domain.Identity;
+using CH.Domain.Queries;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CH.Test.QueryTests
+{
+    [TestClass]
+    public class UserQueryHandlerTests : BaseTest
+    {
+        [TestMethod]
+        public async Task ReturnsUser()
+        {
+            UserQuery query = new UserQuery()
+            {
+                Username="unit.test"
+            };
+
+            IdentityUser user = new IdentityUser(query.Username);
+
+            Mock<IApplicationUserStore> mockUserStore = new Mock<IApplicationUserStore>();
+            mockUserStore.Setup(x => x.FindByNameAsync(query.Username)).Returns(Task.FromResult(user));
+
+            UserQueryHandler handler = new UserQueryHandler(mockUserStore.Object);
+            (await handler.Retrieve(query)).Should().Equals(user);
+
+            mockUserStore.Verify(x => x.FindByNameAsync(query.Username), Times.Once);
+        }
+    }
+}
