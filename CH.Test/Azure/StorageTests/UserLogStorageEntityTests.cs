05b9be1aa40a10749be6a901cdc5437af387328a
Tim <tduncan72@gmail.com>
2014-12-27 18:28:08 -0500

    Added missed files

---
+++ CH.Test/Azure/StorageTests/UserLogStorageEntityTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CH.Azure.Storage.Logging;
+using CH.Domain.Models.Logging;
+using CH.Domain.Proxies.Configuration;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH.Test.Azure.StorageEntityTests
+{
+    [TestClass]
+    public class UserLogStorageEntityTests : BaseAzureTest
+    {
+        [TestMethod]
+        public void SuccessfullyMapsEntityToAndFromStorage()
+        {
+            UserLog userLog = new UserLog(UserActions.PasswordLoginSuccess, "username", DateTime.UtcNow);
+            userLog.AdditionalData = "data";
+
+            AzureUserLogger source = new AzureUserLogger( new AzureConfiguration());
+            AzureUserLogger target = new AzureUserLogger(new AzureConfiguration());
+            UserLog result = target.FromStorage(source.ToStorage(userLog));
+
+            result.ID.Should().Be(userLog.ID);
+            result.Action.Should().Be(userLog.Action);
+            result.Username.Should().Be(userLog.Username);
+            result.WhenOccurredUtc.Should().Be(userLog.WhenOccurredUtc);
+            result.WhenOccurredUtc.Kind.Should().Be(DateTimeKind.Utc);
+            result.AdditionalData.Should().Be(userLog.AdditionalData);
+        }
+    }
+}
