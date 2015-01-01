db72b5b30c26eae31428f4d2069fbf70fb01872f
Tim <tduncan72@gmail.com>
2014-12-31 21:00:51 -0500

    Added unit test

---
+++ CH.Test/ControllerTests/DataMaintenanceControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CH.Website.Areas.Admin.Controllers;
+using CH.Website.Areas.Admin.Models;
+using CH.Website.Sources.Storage;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Utility.StringHelpers;
+
+namespace CH.Test.ControllerTests
+{
+    [TestClass]
+    public class DataMaintenanceControllerTests
+    {
+        [TestMethod]
+        public void GetDashboardReturnsView()
+        {
+            DataMaintenanceController controller = new DataMaintenanceController(null,null);
+
+            ViewResult viewResult = controller.Dashboard();
+            viewResult.Should().NotBeNull();
+
+            DashboardModel model = viewResult.Model as DashboardModel;
+            model.Should().NotBeNull();
+
+            model.TargetStorageItem.StorageSourceID.Should().Be(new AzureStorageSource().ID);
+            model.TargetStorageItem.Title.Should().NotBeNullOrEmpty();
+
+            model.SourceStorageItem.StorageSourceID.Should().Be(new RescueGroupsStorageSource().ID);
+            model.SourceStorageItem.Title.Should().NotBeNullOrEmpty();
+
+            model.Items.Should().NotBeNullOrEmpty();
+            model.Items.Any(x => x.Title.IsNullOrEmpty()).Should().BeFalse();
+        }
+    }
+}
