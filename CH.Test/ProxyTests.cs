8823165469c66731cb1c85ce9a44cbaa204d3c23
Tim <tduncan72@gmail.com>
2015-01-12 20:36:24 -0500

    Added interface and proxy class for IPrincipal from IOwinContext.Request

---
+++ CH.Test/ProxyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using CritterHeroes.Web.Common.Identity;
+using CritterHeroes.Web.Common.Proxies;
+using FluentAssertions;
+using Microsoft.Owin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CH.Test
+{
+    [TestClass]
+    public class ProxyTests
+    {
+        [TestMethod]
+        public void HttpUserProxyReturnsCorrectProperties()
+        {
+            GenericIdentity genericIdentity = new GenericIdentity("user.name");
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(genericIdentity);
+            claimsIdentity.AddClaim(new Claim(AppClaimTypes.UserID, "userid"));
+
+            Mock<IOwinContext> mockOwinContext = new Mock<IOwinContext>();
+            mockOwinContext.Setup(x => x.Request.User).Returns(new ClaimsPrincipal(claimsIdentity));
+
+            HttpUserProxy userProxy = new HttpUserProxy(mockOwinContext.Object);
+
+            userProxy.Username.Should().Be(genericIdentity.Name);
+            userProxy.UserID.Should().Be("userid");
+        }
+    }
+}
---
+++ CritterHeroes.Web/Contracts/IHttpUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CritterHeroes.Web.Contracts
+{
+    public interface IHttpUser
+    {
+        string Username
+        {
+            get;
+        }
+
+        string UserID
+        {
+            get;
+        }
+
+        bool IsInRole(string role);
+    }
+}
