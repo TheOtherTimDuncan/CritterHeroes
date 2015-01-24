2cf793151630cfb0b476de92c81c2b6061314448
Tim <tduncan72@gmail.com>
2015-01-24 18:59:30 -0500

    Upgraded jQuery from 2.1.1 to 2.1.3

--- CritterHeroes.Web/App_Start/BundleConfig.cs
+++ CritterHeroes.Web/App_Start/BundleConfig.cs
@@ -41,8 +41,8 @@ namespace CritterHeroes.Web
         public static void RegisterjQuery(BundleCollection bundles)
         {
             // jQuery
-            Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js")
-                .Include("~/Scripts/jquery-2.1.1.*");
+            Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.3.min.js")
+                .Include("~/Scripts/jquery-2.1.3.*");
             bundles.Add(jqueryBundle);
 
             // jQuery validation
