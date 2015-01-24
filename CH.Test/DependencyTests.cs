f1c2e21b2b3a4720d18d13cba59a151f95fff739
Tim <tduncan72@gmail.com>
2015-01-24 18:54:57 -0500

    Upgraded SimpleInjector from 2.6.1 to 2.7.0 Removed unneeded tests of dependency container

--- CH.Test/DependencyTests.cs
+++ CH.Test/DependencyTests.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+using System.Web.Mvc;
 using CritterHeroes.Web;
-using CritterHeroes.Web.Common.Dispatchers;
 using CritterHeroes.Web.Contracts.Dashboard;
-using CritterHeroes.Web.Contracts.Notifications;
-using CritterHeroes.Web.Contracts.Queries;
 using CritterHeroes.Web.Contracts.Storage;
 using CritterHeroes.Web.Models.Data;
 using FluentAssertions;
@@ -26,45 +22,17 @@ namespace CH.Test
             Container container = DIConfig.ConfigureDependencyContainer();
             container.Verify();
 
-            IEnumerable<DiagnosticResult> results = Analyzer.Analyze(container);
+            IEnumerable<DiagnosticResult> results = Analyzer.Analyze(container).Where(x =>
+            {
+                if (x.DiagnosticType == DiagnosticType.DisposableTransientComponent && typeof(Controller).IsAssignableFrom(x.ServiceType))
+                {
+                    // Ignore Transient lifestyle for IDisposable warning for controllers
+                    return false;
+                }
+
+                return true;
+            });
             results.Any().Should().BeFalse(string.Join(Environment.NewLine, results.Select(x => x.Description)));
-        }
-
-        [TestMethod]
-        public async Task DependencyContainerCanResolveQueryHandlers()
-        {
-            Container container = DIConfig.ConfigureDependencyContainer(Assembly.GetExecutingAssembly());
-            QueryDispatcher dispatcher = new QueryDispatcher(container);
-
-            TestQuery testQuery = new TestQuery()
-            {
-                TestValue = "testvalue"
-            };
-            TestResult testResult = dispatcher.Dispatch(testQuery);
-            testResult.Should().NotBeNull();
-            testResult.Result.Should().Be(testQuery.TestValue);
-
-            TestAsyncQuery testAsyncQuery = new TestAsyncQuery()
-            {
-                TestValue = "testasyncvalue"
-            };
-            TestResult testAsyncResult = await dispatcher.DispatchAsync(testAsyncQuery);
-            testAsyncResult.Should().NotBeNull();
-            testAsyncResult.Result.Should().Be(testAsyncQuery.TestValue);
-        }
-
-        [TestMethod]
-        public async Task DependencyContainerCanResolveNotificationHandlers()
-        {
-            Container container = DIConfig.ConfigureDependencyContainer(Assembly.GetExecutingAssembly());
-            NotificationPublisher publisher = new NotificationPublisher(container);
-
-            TestNotification notification = new TestNotification();
-
-            publisher.Publish(notification);
-            await publisher.PublishAsync(notification);
-
-            notification.Value.Should().Be(4, "there are 2 synchronous and 2 asynchronous handlers registered for this notification");
         }
 
         [TestMethod]
@@ -84,96 +52,6 @@ namespace CH.Test
             InstanceProducer producer = container.GetRegistration(typeof(IDashboardStatusCommandHandler<Breed>));
             producer.Should().NotBeNull();
             producer.Registration.ImplementationType.Should().Be(typeof(CritterHeroes.Web.Areas.Admin.DataMaintenance.CommandHandlers.BreedDashboardStatusCommandHandler));
-        }
-    }
-
-    public class TestResult
-    {
-        public string Result
-        {
-            get;
-            set;
-        }
-    }
-
-    public class TestQuery : IQuery<TestResult>
-    {
-        public string TestValue
-        {
-            get;
-            set;
-        }
-    }
-
-    public class TestAsyncQuery : IAsyncQuery<TestResult>
-    {
-        public string TestValue
-        {
-            get;
-            set;
-        }
-    }
-
-    public class TestQueryHandler : IQueryHandler<TestQuery, TestResult>
-    {
-        public TestResult Retrieve(TestQuery query)
-        {
-            return new TestResult()
-            {
-                Result = query.TestValue
-            };
-        }
-    }
-
-    public class TestAsyncQueryHandler : IAsyncQueryHandler<TestAsyncQuery, TestResult>
-    {
-        public Task<TestResult> RetrieveAsync(TestAsyncQuery query)
-        {
-            return Task.FromResult(new TestResult()
-            {
-                Result = query.TestValue
-            });
-        }
-    }
-
-    public class TestNotification : INotification, IAsyncNotification
-    {
-        public int Value
-        {
-            get;
-            set;
-        }
-    }
-
-    public class TestNotificationHandler1 : INotificationHandler<TestNotification>
-    {
-        void INotificationHandler<TestNotification>.Execute(TestNotification notification)
-        {
-            notification.Value++;
-        }
-    }
-
-    public class TestNotificationHandler2 : INotificationHandler<TestNotification>
-    {
-        void INotificationHandler<TestNotification>.Execute(TestNotification notification)
-        {
-            notification.Value++;
-        }
-    }
-
-    public class TestAsyncNotificationHandler1 : IAsyncNotificationHandler<TestNotification>
-    {
-        public async Task ExecuteAsync(TestNotification notification)
-        {
-            notification.Value++;
-        }
-    }
-
-    public class TestAsyncNotificationHandler2 : IAsyncNotificationHandler<TestNotification>
-    {
-        public async Task ExecuteAsync(TestNotification notification)
-        {
-            notification.Value++;
         }
     }
 }
