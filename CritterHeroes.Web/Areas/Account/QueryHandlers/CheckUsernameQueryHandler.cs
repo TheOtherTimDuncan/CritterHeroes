312be9a87676a984de14f7f8ca2ca035f6829e41
Tim <tduncan72@gmail.com>
2015-01-25 14:18:05 -0500

    Added extension method for IOwinRequest Changed CheckUsernameQueryHandler to use IOwinContext

--- CritterHeroes.Web/Areas/Account/QueryHandlers/CheckUsernameQueryHandler.cs
+++ CritterHeroes.Web/Areas/Account/QueryHandlers/CheckUsernameQueryHandler.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using CritterHeroes.Web.Areas.Account.Models;
 using CritterHeroes.Web.Common.Identity;
+using CritterHeroes.Web.Common.OwinExtensions;
 using CritterHeroes.Web.Common.Queries;
-using CritterHeroes.Web.Contracts;
 using CritterHeroes.Web.Contracts.Identity;
 using CritterHeroes.Web.Contracts.Logging;
 using CritterHeroes.Web.Contracts.Queries;
 using CritterHeroes.Web.Models.Logging;
+using Microsoft.Owin;
 using TOTD.Utility.ExceptionHelpers;
 
 namespace CritterHeroes.Web.Areas.Account.QueryHandlers
@@ -18,13 +19,13 @@ namespace CritterHeroes.Web.Areas.Account.QueryHandlers
     {
         public IUserLogger _userLogger;
         public IApplicationUserStore _userStore;
-        public IHttpContext _httpContext;
+        private IOwinContext _owinContext;
 
-        public CheckUsernameQueryHandler(IUserLogger userLogger, IApplicationUserStore userStore, IHttpContext httpContext)
+        public CheckUsernameQueryHandler(IUserLogger userLogger, IApplicationUserStore userStore, IOwinContext owinContext)
         {
             this._userLogger = userLogger;
             this._userStore = userStore;
-            this._httpContext = httpContext;
+            this._owinContext = owinContext;
         }
 
         public async Task<CheckUsernameResult> RetrieveAsync(UsernameQuery query)
@@ -34,10 +35,10 @@ namespace CritterHeroes.Web.Areas.Account.QueryHandlers
             var logData = new
             {
                 Request = query.Username,
-                RequestFrom = _httpContext.Request.UrlReferrer.AbsoluteUri,
+                RequestFrom = _owinContext.Request.GetReferrer(),
             };
 
-            await _userLogger.LogActionAsync(UserActions.DuplicateUsernameCheck, _httpContext.User.Identity.Name, logData);
+            await _userLogger.LogActionAsync(UserActions.DuplicateUsernameCheck, _owinContext.Request.User.Identity.Name, logData);
             IdentityUser user = await _userStore.FindByNameAsync(query.Username);
             return new CheckUsernameResult()
             {
---
+++ CritterHeroes.Web/Common/OwinExtensions/OwinContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+
+namespace CritterHeroes.Web.Common.OwinExtensions
+{
+    public static class OwinContextExtensions
+    {
+        public static string GetReferrer(this IOwinRequest owinRequest)
+        {
+            string[] value;
+            if (owinRequest.Headers.TryGetValue("referer", out value))
+            {
+                return value[0];
+            }
+
+            return null;
+        }
+    }
+}
