40925b784a78a133bd8ee57d1373c4ae6a9f85c7
Tim <tduncan72@gmail.com>
2015-07-12 16:02:51 -0400

    Remove code modal dialog partial view

--- CritterHeroes.Web/Areas/Common/HtmlHelperExtensions.cs
+++ CritterHeroes.Web/Areas/Common/HtmlHelperExtensions.cs
@@ -1,33 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc.Html;
 using CritterHeroes.Web.Areas.Common.ActionExtensions;
-using CritterHeroes.Web.Areas.Home;
-using CritterHeroes.Web.Areas.Models.Modal;
 using CritterHeroes.Web.Common.StateManagement;
 using CritterHeroes.Web.Contracts.StateManagement;
-using CritterHeroes.Web.Middleware;
-using TOTD.Mvc.Actions;
 using TOTD.Utility.Misc;
-using TOTD.Utility.StringHelpers;
 
 namespace CritterHeroes.Web.Areas.Common
 {
     public static class HtmlHelperExtensions
     {
-        public static MvcHtmlString ModalDialog(this HtmlHelper htmlHelper, ModalDialogModel model)
-        {
-            if (model == null)
-            {
-                return null;
-            }
-            return htmlHelper.Partial("_ModalDialog", model);
-        }
-
         public static MvcHtmlString CancelButton(this HtmlHelper htmlHelper)
         {
             TagBuilder builder = new TagBuilder("a");
