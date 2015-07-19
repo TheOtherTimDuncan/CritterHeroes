e9404f99dc6f3aff7a66c93edfb3904bca54d474
Tim <tduncan72@gmail.com>
2015-07-19 18:34:46 -0400

    Add base data context and EF configuration

---
+++ CritterHeroes.Web/Data/Contexts/BaseDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CritterHeroes.Web.Data.Configurations;
+using TOTD.EntityFramework;
+
+namespace CritterHeroes.Web.Data.Contexts
+{
+    [DbConfigurationType(typeof(EntityFrameworkConfiguration))]
+    public class BaseDbContext : DbContext
+    {
+
+        public BaseDbContext(string connectionStringName)
+            : base("name=" + connectionStringName)
+        {
+#if DEBUG
+            if (Debugger.IsAttached)
+            {
+                Database.Log = (string value) => Debug.WriteLine(value);
+            }
+            else
+            {
+                Database.Log = Console.WriteLine;
+            }
+#endif
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                int result = base.SaveChanges();
+                return result;
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw new DbEntityValidationDetailException(ex);
+            }
+        }
+
+        public override async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                int result = await base.SaveChangesAsync();
+                return result;
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw new DbEntityValidationDetailException(ex);
+            }
+        }
+    }
+}
