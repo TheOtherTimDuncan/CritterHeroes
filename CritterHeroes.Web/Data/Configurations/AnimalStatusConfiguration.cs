942ecffd74e280cfda7c87e86a1ca7d6aa541aa6
Tim <tduncan72@gmail.com>
2015-07-24 19:44:42 -0400

    Fix .gitignore to allow Entity Framework configurations to be added to source control

---
+++ CritterHeroes.Web/Data/Configurations/AnimalStatusConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using CritterHeroes.Web.Data.Models;
+
+namespace CritterHeroes.Web.Data.Configurations
+{
+    public class AnimalStatusConfiguration : EntityTypeConfiguration<AnimalStatus>
+    {
+        public AnimalStatusConfiguration()
+        {
+            HasKey(x => x.ID);
+
+            Property(x => x.ID).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            Property(x => x.Name).HasMaxLength(25).HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute()
+            {
+                IsUnique = true
+            })); ;
+            Property(x => x.Description).HasMaxLength(100);
+        }
+    }
+}
---
+++ CritterHeroes.Web/Data/Configurations/BreedConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using CritterHeroes.Web.Data.Models;
+
+namespace CritterHeroes.Web.Data.Configurations
+{
+    public class BreedConfiguration : EntityTypeConfiguration<Breed>
+    {
+        public BreedConfiguration()
+        {
+            HasKey(x => x.ID);
+
+            Property(x => x.ID).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            Property(x => x.Species).HasMaxLength(20).HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute()));
+            Property(x => x.BreedName).HasMaxLength(50);
+        }
+    }
+}
