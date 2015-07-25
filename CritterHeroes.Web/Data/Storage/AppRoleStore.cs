b2886897d6dedbefdd4db6f6356ecc6f0545f49f
Tim <tduncan72@gmail.com>
2015-07-25 16:20:36 -0400

    Add user and role storage for Identity with Entity Framework

---
+++ CritterHeroes.Web/Data/Storage/AppRoleStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Data.Contexts;
+using CritterHeroes.Web.Data.Models.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CritterHeroes.Web.Data.Storage
+{
+    public class AppRoleStore : RoleStore<AppRole, int, AppUserRole>, IQueryableRoleStore<AppRole, int>, IRoleStore<AppRole, int>, IDisposable
+    {
+        public AppRoleStore(AppUserStorageContext context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ CritterHeroes.Web/Data/Storage/AppUserStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Contracts.Identity;
+using CritterHeroes.Web.Data.Contexts;
+using CritterHeroes.Web.Data.Models.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CritterHeroes.Web.Data.Storage
+{
+    public class AppUserStore : UserStore<AppUser, AppRole, int, AppUserLogin, AppUserRole, AppUserClaim>, IUserStore<AppUser, int>, IDisposable
+    {
+        public AppUserStore(AppUserStorageContext context)
+            : base(context)
+        {
+        }
+    }
+}
