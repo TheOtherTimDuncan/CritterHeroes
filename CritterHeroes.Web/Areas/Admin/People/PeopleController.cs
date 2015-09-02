4e3ed919362257b91014d55b1ff5243e5daba6e3
Tim <tduncan72@gmail.com>
2015-09-02 16:50:46 -0400

    Add starting view for Admin People

---
+++ CritterHeroes.Web/Areas/Admin/People/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CritterHeroes.Web.Areas.Admin.People.Models;
+using CritterHeroes.Web.Contracts.Commands;
+using CritterHeroes.Web.Contracts.Queries;
+using CritterHeroes.Web.Data.Models.Identity;
+
+namespace CritterHeroes.Web.Areas.Admin.People
+{
+    [Authorize(Roles = UserRole.MasterAdmin)]
+    [Route("People/{action=index}")]
+    public class PeopleController : BaseAdminController
+    {
+        public PeopleController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+            : base(queryDispatcher, commandDispatcher)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            PeopleSummaryModel model = new PeopleSummaryModel();
+            return View(model);
+        }
+    }
+}
---
+++ CritterHeroes.Web/Areas/Common/ActionExtensions/AdminPeopleActionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CritterHeroes.Web.Areas.Admin.People;
+using TOTD.Mvc.Actions;
+using TOTD.Mvc.FluentHtml.Elements;
+
+namespace CritterHeroes.Web.Areas.Common.ActionExtensions
+{
+    public static class AdminPeopleActionExtensions
+    {
+        public static LinkElement AdminPeopleActionLink(this LinkElement linkElement, Expression<Func<PeopleController, ActionResult>> actionSelector)
+        {
+            return linkElement.ActionLink<PeopleController>(actionSelector);
+        }
+    }
+}
