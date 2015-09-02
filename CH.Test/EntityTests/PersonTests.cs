b6375d1441689d9db47ee54f425ec1c0c5f4db17
Tim <tduncan72@gmail.com>
2015-09-02 12:05:00 -0400

    Add test for Person entity

---
+++ CH.Test/EntityTests/PersonTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CritterHeroes.Web.Data.Contexts;
+using CritterHeroes.Web.Data.Extensions;
+using CritterHeroes.Web.Data.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.EntityFramework;
+
+namespace CH.Test.EntityTests
+{
+    [TestClass]
+    public class PersonTests : BaseTest
+    {
+        [TestMethod]
+        public async Task CanCreateReadAndDeletePerson()
+        {
+            // Use a separate context for saving vs retrieving to prevent any caching
+
+            State state = new State("OH", "Ohio");
+            using (SqlStorageContext<State> storageContext = new SqlStorageContext<State>())
+            {
+                storageContext.Add(state);
+                await storageContext.SaveChangesAsync();
+            }
+            Person person = new Person()
+            {
+                State = state.Abbreviation
+            };
+
+            using (SqlStorageContext<Person> storageContext = new SqlStorageContext<Person>())
+            {
+                EntityTestHelper.FillWithTestData(storageContext, person, "ID", "State");
+                storageContext.Add(person);
+                await storageContext.SaveChangesAsync();
+            }
+
+            using (SqlStorageContext<Person> storageContext = new SqlStorageContext<Person>())
+            {
+                Person result = await storageContext.Entities.FindByIDAsync(person.ID);
+                result.Should().NotBeNull();
+
+                result.FirstName.Should().Be(person.FirstName);
+                result.LastName.Should().Be(person.LastName);
+                result.Email.Should().Be(person.Email);
+                result.Address.Should().Be(person.Address);
+                result.City.Should().Be(person.City);
+                result.Zip.Should().Be(person.Zip);
+                result.State.Should().Be(person.State);
+                result.RescueGroupsID.Should().Be(person.RescueGroupsID);
+
+                storageContext.Delete(result);
+                await storageContext.SaveChangesAsync();
+
+                storageContext.Entities.MatchingID(person.ID).SingleOrDefault().Should().BeNull();
+            }
+        }
+    }
+}
---
+++ CritterHeroes.Web/Data/Extensions/PersonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using CritterHeroes.Web.Data.Models;
+using System.Threading.Tasks;
+
+namespace CritterHeroes.Web.Data.Extensions
+{
+    public static class PersonExtensions
+    {
+        public static IQueryable<Person> MatchingID(this IQueryable<Person> source, int personID)
+        {
+            return source.Where(x => x.ID == personID);
+        }
+
+        public async static Task<Person> FindByIDAsync(this IQueryable<Person> source, int personID)
+        {
+            return await source.MatchingID(personID).SingleOrDefaultAsync();
+        }
+    }
+}
