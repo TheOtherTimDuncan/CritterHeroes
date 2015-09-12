98d4ce43fa5ee02811fddcde134df422d9afaa91
Tim <tduncan72@gmail.com>
2015-09-12 17:43:53 -0400

    Change HomeActionExtensions to extend non-expression based methods

--- CritterHeroes.Web/Areas/Common/ActionExtensions/HomeActionExtensions.cs
+++ CritterHeroes.Web/Areas/Common/ActionExtensions/HomeActionExtensions.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 using CritterHeroes.Web.Areas.Home;
 using TOTD.Mvc.Actions;
-using TOTD.Mvc.FluentHtml.Elements;
 
 namespace CritterHeroes.Web.Areas.Common.ActionExtensions
 {
     public static class HomeActionExtensions
     {
-        public static LinkElement HomeActionLink(this LinkElement linkElement, Expression<Func<HomeController, ActionResult>> actionSelector)
+        public static void RenderHomeMenuAction(this HtmlHelper htmlHelper)
         {
-            return linkElement.ActionLink<HomeController>(actionSelector);
+            htmlHelper.RenderAction(nameof(HomeController.Menu), ControllerRouteName);
         }
 
-        public static void RenderHomeAction(this HtmlHelper htmlHelper, Expression<Func<HomeController, ActionResult>> actionSelector)
+        public static void RenderHomeHeaderAction(this HtmlHelper htmlHelper)
         {
-            htmlHelper.RenderAction(actionSelector, null);
+            htmlHelper.RenderAction(nameof(HomeController.Header), ControllerRouteName);
         }
 
-        public static string HomeAction(this UrlHelper urlHelper, Expression<Func<HomeController, ActionResult>> actionSelector)
+        private static string ControllerRouteName
         {
-            return urlHelper.Action<HomeController>(actionSelector);
-        }
+            get;
+        } = ActionHelper.GetControllerRouteName(nameof(HomeController));
     }
 }
