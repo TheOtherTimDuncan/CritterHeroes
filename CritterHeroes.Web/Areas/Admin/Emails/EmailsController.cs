4d8e2fea4a797024b9e2dd774a65537ca39cb29b
Tim <tduncan72@gmail.com>
2015-10-18 15:53:37 -0400

    Add starting view for page to send admin email

---
+++ CritterHeroes.Web/Areas/Admin/Emails/EmailsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CritterHeroes.Web.Contracts.Commands;
+using CritterHeroes.Web.Contracts.Queries;
+using CritterHeroes.Web.Data.Models.Identity;
+
+namespace CritterHeroes.Web.Areas.Admin.Emails
+{
+    [Authorize(Roles = UserRole.MasterAdmin)]
+    [Route("Emails/{action=index}")]
+    public class EmailsController : BaseAdminController
+    {
+        public EmailsController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+            : base(queryDispatcher, commandDispatcher)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CritterHeroes.Web/Areas/Common/ActionExtensions/EmailActionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Areas.Admin.Emails;
+using TOTD.Mvc.Actions;
+using TOTD.Mvc.FluentHtml.Elements;
+
+namespace CritterHeroes.Web.Areas.Common.ActionExtensions
+{
+    public static class EmailActionExtensions
+    {
+        public static LinkElement AdminEmailHomeActionLink(this LinkElement linkElement)
+        {
+            return linkElement.ActionLink(nameof(EmailsController.Index), ControllerRouteName, AreaName.AdminRouteValue);
+        }
+
+        public static string ControllerRouteName
+        {
+            get;
+        } = ActionHelper.GetControllerRouteName(nameof(EmailsController));
+    }
+}
