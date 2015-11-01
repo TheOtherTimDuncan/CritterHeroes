57a722f4fd2d5294b296fec991f55b228f4f3f35
Tim <tduncan72@gmail.com>
2015-11-01 14:12:13 -0500

    Add MockFileSystem

---
+++ CH.Test/Mocks/MockFileSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CritterHeroes.Web.Contracts;
+using Moq;
+
+namespace CH.Test.Mocks
+{
+    public class MockFileSystem:Mock<IFileSystem>
+    {
+        public MockFileSystem()
+        {
+            this.Setup(x => x.CombinePath(It.IsAny<string[]>())).Returns((string[] paths) => Path.Combine(paths));
+        }
+    }
+}
