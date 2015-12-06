5e45c97a35d876b4c5827102a0b52f7027d250bf
Tim <tduncan72@gmail.com>
2015-12-06 16:29:34 -0500

    Add SelectOptionModel

---
+++ CritterHeroes.Web/Areas/Common/Models/SelectOptionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CritterHeroes.Web.Areas.Common.Models
+{
+    public class SelectOptionModel
+    {
+        public int Value
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
