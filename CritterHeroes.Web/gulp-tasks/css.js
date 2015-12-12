a5696937cd8e5088481f38a52f020270b22544fc
Tim <tduncan72@gmail.com>
2015-12-12 15:43:53 -0500

    Change gulp css task to not version debug css and streamlined build process

--- CritterHeroes.Web/gulp-tasks/css.js
+++ CritterHeroes.Web/gulp-tasks/css.js
@@ -12,29 +12,9 @@ module.exports = function (gulp, plugins, common) {
     var srcImages = common.srcPath + '/images';
 
     var libBootstrap = srcLess + '/bootstrap';
-    var stagingCss = srcLess + '/staging';
-
-    var hashes = {};
-
-    var getHashes = function () {
-
-        var collect = function (file, enc, cb) {
-            if (file.revHash) {
-                hashes[common.path.basename(file.revOrigPath, common.path.extname(file.revOrigPath))] = file.revHash;
-            }
-            this.push(file);
-            return cb();
-        };
-
-        return plugins.through2.obj(collect);
-    };
 
     gulp.task('clean-css', function () {
         return plugins.del([distCss + '/**', distFonts + '/**', distImages + '/**/' + '!' + common.distPath, libBootstrap, '!' + srcLess, './versioned-css.json']);
-    });
-
-    gulp.task('clean-css-staging', ['app-css'], function () {
-        return plugins.del(stagingCss);
     });
 
     gulp.task('copy-images', ['clean-css'], function () {
@@ -65,38 +45,24 @@ module.exports = function (gulp, plugins, common) {
 
     });
 
-    gulp.task('stage-css', ['clean-css', 'copy-bootstrap', 'copy-bootswatch'], function () {
+    gulp.task('app-css', ['clean-css', 'copy-bootstrap', 'copy-bootswatch'], function () {
 
         var autoprefix = new lessPluginAutoPrefix({ browsers: ['last 2 versions'] });
 
         return gulp.src(srcLess + '/*.less')
             .pipe(plugins.sourcemaps.init())
             .pipe(plugins.less({ plugins: [autoprefix] }))
+            .pipe(gulp.dest(distCss))
+            .pipe(plugins.minifyCss())
+            .pipe(plugins.rename({ extname: '.min.css' }))
             .pipe(plugins.rev())
-            .pipe(getHashes())
-            .pipe(plugins.sourcemaps.write('../../../' + distCss))
-            .pipe(gulp.dest(stagingCss))
-            .pipe(plugins.rev.manifest({ path: "versioned-css.json" }))
+            .pipe(plugins.sourcemaps.write('.'))
+            .pipe(gulp.dest(distCss))
+            .pipe(plugins.rev.manifest("versioned-css.json"))
             .pipe(gulp.dest('./'));
 
     });
 
-    gulp.task('app-css', ['clean-css', 'stage-css'], function () {
-
-        return gulp.src(stagingCss + '/*.css')
-            .pipe(plugins.plumber({
-                errorHandler: function (err) {
-                    console.log(err);
-                    this.emit('end');
-                }
-            }))
-            .pipe(gulp.dest(distCss))
-            .pipe(plugins.minifyCss())
-            .pipe(plugins.rename({ extname: '.min.css' }))
-            .pipe(gulp.dest(distCss));
-
-    });
-
-    return ['clean-css', 'copy-images', 'copy-bootstrap', 'copy-bootswatch', 'copy-bootstrap-fonts', 'stage-css', 'app-css', 'clean-css-staging'];
+    return ['clean-css', 'copy-images', 'copy-bootstrap', 'copy-bootswatch', 'copy-bootstrap-fonts', 'app-css'];
 
 };
