aab98c9239df3fb846c22305e6c1ac384bc63bfb
Tim <tduncan72@gmail.com>
2015-12-13 18:47:13 -0500

    Wrap dependency test in using block to ensure container is disposed

--- CH.Test/DependencyTests.cs
+++ CH.Test/DependencyTests.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using CritterHeroes.Web;
-using CritterHeroes.Web.Contracts.Dashboard;
-using CritterHeroes.Web.Contracts.Storage;
-using CritterHeroes.Web.Data.Models;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleInjector;
@@ -19,20 +16,22 @@ namespace CH.Test
         [TestMethod]
         public void DependencyContainerIsValid()
         {
-            Container container = DIConfig.ConfigureDependencyContainer();
-            container.Verify();
-
-            IEnumerable<DiagnosticResult> results = Analyzer.Analyze(container).Where(x =>
+            using (Container container = DIConfig.ConfigureDependencyContainer())
             {
-                if (x.DiagnosticType == DiagnosticType.DisposableTransientComponent && typeof(Controller).IsAssignableFrom(x.ServiceType))
-                {
-                    // Ignore Transient lifestyle for IDisposable warning for controllers
-                    return false;
-                }
+                container.Verify();
 
-                return true;
-            });
-            results.Any().Should().BeFalse(string.Join(Environment.NewLine, results.Select(x => x.Description)));
+                IEnumerable<DiagnosticResult> results = Analyzer.Analyze(container).Where(x =>
+                {
+                    if (x.DiagnosticType == DiagnosticType.DisposableTransientComponent && typeof(Controller).IsAssignableFrom(x.ServiceType))
+                    {
+                        // Ignore Transient lifestyle for IDisposable warning for controllers
+                        return false;
+                    }
+
+                    return true;
+                });
+                results.Should().BeNullOrEmpty(string.Join(Environment.NewLine, results.Select(x => x.Description)));
+            }
         }
     }
 }
