ee5515345413100b9adb0f44a504eb634a8bd3bb
Tim <tduncan72@gmail.com>
2015-12-14 19:00:36 -0500

    Add javascript pubsub

---
+++ CritterHeroes.Web/src/js/bundled/ch.pubsub.js
@@ -0,0 +1,39 @@
+﻿(function (cheroes) {
+
+    'use strict';
+
+    var queue = [];
+
+    function publish(eventName, data) {
+
+        if (queue[eventName]) {
+            for (var i = 0; i < queue[eventName].length; i++) {
+                queue[eventName][i].callback(data);
+            }
+        }
+
+    }
+
+    function subscribe(eventName, callback) {
+
+        if (!queue[eventName]) {
+            queue[eventName] = [];
+        }
+
+        queue[eventName].push(
+        {
+            callback: callback
+        });
+
+    }
+
+    var events = {
+    };
+
+    cheroes.pubsub = {
+        events: events,
+        publish: publish,
+        subscribe: subscribe
+    };
+
+}(this.cheroes = this.cheroes || {}));
