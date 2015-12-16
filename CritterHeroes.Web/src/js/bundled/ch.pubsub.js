146f89482c9774c8426799621a512b3f468f2c78
Tim <tduncan72@gmail.com>
2015-12-15 19:07:01 -0500

    Change javascript pubsub to accept context data for subscribing and add comments

--- CritterHeroes.Web/src/js/bundled/ch.pubsub.js
+++ CritterHeroes.Web/src/js/bundled/ch.pubsub.js
@@ -4,34 +4,53 @@
 
     var queue = [];
 
+    /**
+      * @param {string}  eventName  - The name of the event to be published
+      * @param           data       - The optional data to be passed to the event subscribers
+      */
     function publish(eventName, data) {
 
-        if (queue[eventName]) {
-            for (var i = 0; i < queue[eventName].length; i++) {
-                queue[eventName][i].callback(data);
+        var eventQueue = queue[eventName];
+
+        if (eventQueue) {
+            for (var i = 0; i < eventQueue.length; i++) {
+                var subscriber = eventQueue[i];
+                subscriber.callback(data, subscriber.context);
             }
         }
 
     }
 
-    function subscribe(eventName, callback) {
+    /**
+     * Callback for subscribe
+     * 
+     * @callback subscribeCallback
+     * @param {object}  options
+     * @param           options.data    - Each event determines what is passed in data
+     * @param           options.context - The value passed to options.context when subscribing
+     */
+
+    /**
+     * @param {string}              eventName           - The name of the event to subscribe to
+     * @param {object}              options             - The options for subscribing to the event
+     * @param {subscribeCallback}   options.callback    - The function to call when the event is published.
+     * @param                       options.context     - Optional value to be passed to the callback function when the event is published
+     */
+    function subscribe(eventName, options) {
 
         if (!queue[eventName]) {
             queue[eventName] = [];
         }
 
-        queue[eventName].push(
-        {
-            callback: callback
-        });
+        queue[eventName].push(options);
 
     }
 
-    var events = {
+    cheroes.events = {
     };
 
+
     cheroes.pubsub = {
-        events: events,
         publish: publish,
         subscribe: subscribe
     };
