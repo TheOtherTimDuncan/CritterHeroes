3c90e7af0517e65f9645df445fc65f73a64d643f
Tim <tduncan72@gmail.com>
2015-12-19 18:30:16 -0500

    Add tests for HomeController

---
+++ CH.Test/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CH.Test.ControllerTests.TestHelpers;
+using CritterHeroes.Web.Areas.Home;
+using CritterHeroes.Web.Areas.Home.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH.Test.ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTests : BaseTest
+    {
+        [TestMethod]
+        public void MenuGetReturnsPartialViewWithModel()
+        {
+            MenuModel model = new MenuModel();
+
+            ControllerTester.UsingController<HomeController>()
+                .SetupQueryDispatcher(model)
+                .WithCallTo(x => x.Menu())
+                .VerifyQueryDispatcher()
+                .ShouldReturnPartialViewResult()
+                .HavingModel(model);
+        }
+
+        [TestMethod]
+        public void HeaderGetReturnsPartialViewWithModel()
+        {
+            HeaderModel model = new HeaderModel();
+
+            ControllerTester.UsingController<HomeController>()
+                .SetupQueryDispatcher(model)
+                .WithCallTo(x => x.Header())
+                .VerifyQueryDispatcher()
+                .ShouldReturnPartialViewResult()
+                .HavingModel(model);
+        }
+    }
+}
