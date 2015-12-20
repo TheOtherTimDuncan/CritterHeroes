6892f922d434aee06968c2bbeb3beaebd37a27ea
Tim <tduncan72@gmail.com>
2015-12-20 12:20:22 -0500

    Add tests for CrittersController

---
+++ CH.Test/ControllerTests/CritttersControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CH.Test.ControllerTests.TestHelpers;
+using CritterHeroes.Web.Areas.Admin.Critters;
+using CritterHeroes.Web.Areas.Admin.Critters.Models;
+using CritterHeroes.Web.Areas.Admin.Critters.Queries;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH.Test.ControllerTests
+{
+    [TestClass]
+    public class CritttersControllerTests : BaseTest
+    {
+        [TestMethod]
+        public void IndexGetReturnsViewWithModel()
+        {
+            CrittersModel model = new CrittersModel();
+            CrittersQuery query = new CrittersQuery();
+
+            ControllerTester.UsingController<CrittersController>()
+                .SetupQueryDispatcherAsync(model, query)
+                .WithCallTo(x => x.Index(query))
+                .VerifyQueryDispatcher()
+                .ShouldReturnViewResult()
+                .HavingModel(model);
+        }
+
+        [TestMethod]
+        public void ListGetReturnsJson()
+        {
+            CrittersListQuery query = new CrittersListQuery();
+            CrittersListModel model = new CrittersListModel();
+
+            ControllerTester.UsingController<CrittersController>()
+                .SetupQueryDispatcherAsync(model, query)
+                .WithCallTo(x => x.List(query))
+                .VerifyQueryDispatcher()
+                .ShouldReturnJsonCamelCase()
+                .HavingModel(model);
+        }
+
+        [TestMethod]
+        public void SummaryGetReturnsViewWithModel()
+        {
+            CritterSummaryModel model = new CritterSummaryModel();
+
+            ControllerTester.UsingController<CrittersController>()
+                .SetupQueryDispatcherAsync(model)
+                .WithCallTo(x => x.Summary())
+                .VerifyQueryDispatcher()
+                .ShouldReturnViewResult()
+                .HavingModel(model);
+        }
+    }
+}
