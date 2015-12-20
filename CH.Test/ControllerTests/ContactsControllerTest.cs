3d534fe2cfa9b2c3cc3b9e3a0ea2b2a882e4fa28
Tim <tduncan72@gmail.com>
2015-12-20 12:14:32 -0500

    Add tests for ContactsController

---
+++ CH.Test/ControllerTests/ContactsControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CH.Test.ControllerTests.TestHelpers;
+using CritterHeroes.Web.Areas.Admin.Contacts;
+using CritterHeroes.Web.Areas.Admin.Contacts.Models;
+using CritterHeroes.Web.Areas.Admin.Contacts.Queries;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH.Test.ControllerTests
+{
+    [TestClass]
+    public class ContactsControllerTest : BaseTest
+    {
+        [TestMethod]
+        public void IndexGetReturnsViewWithModel()
+        {
+            ContactsModel model = new ContactsModel();
+            ContactsQuery query = new ContactsQuery();
+
+            ControllerTester.UsingController<ContactsController>()
+                .SetupQueryDispatcherAsync(model, query)
+                .WithCallTo(x => x.Index(query))
+                .VerifyQueryDispatcher()
+                .ShouldReturnViewResult()
+                .HavingModel(model);
+        }
+
+        [TestMethod]
+        public void ListGetReturnsJson()
+        {
+            ContactsListModel model = new ContactsListModel();
+            ContactsListQuery query = new ContactsListQuery();
+
+            ControllerTester.UsingController<ContactsController>()
+                .SetupQueryDispatcherAsync(model, query)
+                .WithCallTo(x => x.List(query))
+                .VerifyQueryDispatcher()
+                .ShouldReturnJsonCamelCase()
+                .HavingModel(model);
+        }
+    }
+}
