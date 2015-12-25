0a6f00ff3ed4b79a6e9667148d219a81dbc8029f
Tim <tduncan72@gmail.com>
2015-12-25 13:33:34 -0500

    Add javascript tests for dataManager

---
+++ CritterHeroes.Web/src/js-tests/ch.data.tests.js
@@ -0,0 +1,41 @@
+﻿(function (cheroes, $, should) {
+
+    'use strict';
+
+    describe('dataManager', function () {
+
+        it('getHtml should execute an http get with the correct parameters', function () {
+
+            var originalAjax = $.ajax;
+
+            $.ajax = function (options) {
+                options.dataType.should.equal('html');
+                options.type.should.equal('GET');
+
+            };
+
+            cheroes.dataManager.getHtml();
+
+            $.ajax = originalAjax;
+
+        });
+
+        it('sendRequest should execute an http post with the correct parameters', function () {
+
+            var originalAjax = $.ajax;
+
+            $.ajax = function (options) {
+                options.dataType.should.equal('json');
+                options.type.should.equal('POST');
+
+            };
+
+            cheroes.dataManager.sendRequest();
+
+            $.ajax = originalAjax;
+
+        });
+
+    });
+
+}(this.cheroes = this.cheroes || {}, jQuery, should));
