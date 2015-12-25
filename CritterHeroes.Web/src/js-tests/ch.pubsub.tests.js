d131fc36dca4ff4b12f1fc9da381448776679e96
Tim <tduncan72@gmail.com>
2015-12-25 12:57:29 -0500

    Add javascript test for pubsub

---
+++ CritterHeroes.Web/src/js-tests/ch.pubsub.tests.js
@@ -0,0 +1,32 @@
+﻿(function (cheroes, should) {
+
+    'use strict';
+
+    describe('pubsub', function () {
+
+        it('publish should include context passed to subscribe', function () {
+
+            var event = 'event';
+
+            var context = {
+                test: 'test'
+            };
+
+            var data = {
+                data: 'data'
+            };
+
+            cheroes.pubsub.subscribe(event, {
+                context: context,
+                callback: function (pubData, pubContext) {
+                    pubData.data.should.equal(data.data);
+                    pubContext.test.should.equal(context.test);
+                }
+            });
+
+            cheroes.pubsub.publish(event, data);
+        });
+
+    });
+
+}(this.cheroes = this.cheroes || {}, should));
