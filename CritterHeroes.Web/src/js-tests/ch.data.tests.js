21afb235259cec08bcc9b49087e9aeea616c5804
Tim <tduncan72@gmail.com>
2015-12-26 13:28:15 -0500

    Add additional javascript tests for pubsub and dataManager

--- CritterHeroes.Web/src/js-tests/ch.data.tests.js
+++ CritterHeroes.Web/src/js-tests/ch.data.tests.js
@@ -36,6 +36,86 @@
 
         });
 
+        it('should call badRequest callback for status code 400', function () {
+
+            var originalAjax = $.ajax;
+
+            var testResponse = {
+                status: 400,
+                responseJSON: 'json',
+                statusText: 'statusText'
+            };
+
+            $.ajax = function (options) {
+                options.error(testResponse);
+            };
+
+            cheroes.dataManager.sendRequest({
+                badRequest: function (response, statusText, status) {
+                    response.should.equal(testResponse.responseJSON);
+                    statusText.should.equal(testResponse.statusText);
+                    status.should.equal(testResponse.status);
+                }
+            });
+
+            $.ajax = originalAjax;
+
+        });
+
+        it('should call error callback for non-successful status code other than 400', function () {
+
+            var originalAjax = $.ajax;
+
+            var response = {
+                jqXhr: 'jqXhr',
+                textStatus: 'textStatus',
+                errorThrown: 'errorThrown'
+            };
+
+            $.ajax = function (options) {
+                options.error(response.jqXhr, response.textStatus, response.errorThrown);
+            };
+
+            cheroes.dataManager.sendRequest({
+                error: function (jqXhr, textStatus, errorThrown) {
+                    jqXhr.should.equal(response.jqXhr);
+                    textStatus.should.equal(response.textStatus);
+                    errorThrown.should.equal(response.errorThrown);
+                }
+            });
+
+            $.ajax = originalAjax;
+
+        });
+
+        it('should call success callback for successful ajax request', function () {
+
+            var originalAjax = $.ajax;
+
+            var response = {
+                responseJSON: 'json',
+                jqXhr: {
+                    status: 200
+                },
+                statusText: 'textStatus'
+            };
+
+            $.ajax = function (options) {
+                options.success(response.responseJSON, response.statusText, response.jqXhr);
+            };
+
+            cheroes.dataManager.sendRequest({
+                success: function (data, statusText, statusCode) {
+                    data.should.equal(response.responseJSON);
+                    statusText.should.equal(response.statusText);
+                    statusCode.should.equal(response.jqXhr.status);
+                }
+            });
+
+            $.ajax = originalAjax;
+
+        });
+
     });
 
 }(this.cheroes = this.cheroes || {}, jQuery, should));
--- CritterHeroes.Web/src/js-tests/ch.pubsub.tests.js
+++ CritterHeroes.Web/src/js-tests/ch.pubsub.tests.js
@@ -4,27 +4,42 @@
 
     describe('pubsub', function () {
 
+        it('should call subscriber when event is published', function () {
+
+            var event1 = 'event';
+            var data1 = 'data';
+
+            cheroes.pubsub.subscribe(event, {
+                callback: function (pubData) {
+                    pubData.should.equal(data1);
+                }
+            });
+
+            cheroes.pubsub.publish(event1, data1);
+
+        });
+
         it('publish should include context passed to subscribe', function () {
 
-            var event = 'event';
+            var event2 = 'event';
 
             var context = {
                 test: 'test'
             };
 
-            var data = {
+            var data2 = {
                 data: 'data'
             };
 
             cheroes.pubsub.subscribe(event, {
                 context: context,
                 callback: function (pubData, pubContext) {
-                    pubData.data.should.equal(data.data);
+                    pubData.data.should.equal(data2.data);
                     pubContext.test.should.equal(context.test);
                 }
             });
 
-            cheroes.pubsub.publish(event, data);
+            cheroes.pubsub.publish(event2, data2);
         });
 
     });
