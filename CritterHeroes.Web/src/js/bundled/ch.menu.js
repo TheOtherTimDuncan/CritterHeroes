a76be1de22881107cc7a3f694118bc1a5fd98a2b
Tim <tduncan72@gmail.com>
2016-01-03 15:32:58 -0500

    Wire up menu toggle button

--- CritterHeroes.Web/src/js/bundled/ch.menu.js
+++ CritterHeroes.Web/src/js/bundled/ch.menu.js
@@ -4,4 +4,25 @@
 
     $('[data-toggle="dropdown"]').dropdownify();
 
+    $('[data-toggle="collapse"]')
+        .on('click', function () {
+
+            var target = $($(this).data('target'));
+
+            if (target.hasClass('open')) {
+                target
+                    .attr('aria-expanded', false)
+                    .slideUp(function () {
+                        target.removeClass('open');
+                    });
+            } else {
+                target
+                    .attr('aria-expanded', true)
+                    .slideDown(function () {
+                        target.addClass('open');
+                    });
+            }
+
+        });
+
 }(this.cheroes = this.cheroes || {}, jQuery));
