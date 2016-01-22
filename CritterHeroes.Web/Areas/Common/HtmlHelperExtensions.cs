bcf380407a963e407834640c805c748f75923d8f
Tim <tduncan72@gmail.com>
2016-01-21 19:18:22 -0500

    Add HtmlHelper extensions to get css class and html attributes for inputs

--- CritterHeroes.Web/Areas/Common/HtmlHelperExtensions.cs
+++ CritterHeroes.Web/Areas/Common/HtmlHelperExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
 using System.Web.Mvc;
 using CritterHeroes.Web.Areas.Common.ActionExtensions;
 using CritterHeroes.Web.Common.StateManagement;
@@ -26,6 +28,45 @@ namespace CritterHeroes.Web.Areas.Common
             builder.MergeAttribute("href", previousUrl);
 
             return new MvcHtmlString(builder.ToString());
+        }
+
+        public static string ModelStateClassFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            string name = ExpressionHelper.GetExpressionText(expression);
+            string fullName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
+
+            ModelState modelState;
+            if (htmlHelper.ViewData.ModelState.TryGetValue(fullName, out modelState))
+            {
+                if (modelState.Errors.Count > 0)
+                {
+                    return HtmlHelper.ValidationInputCssClassName;
+                }
+            }
+
+            return null;
+        }
+
+        public static MvcHtmlString ValidationAttributesFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            string name = ExpressionHelper.GetExpressionText(expression);
+            string fullName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
+
+            StringBuilder sb = new StringBuilder();
+
+            var attributes = htmlHelper.GetUnobtrusiveValidationAttributes(name, metadata);
+            foreach (var keyValue in attributes)
+            {
+                sb
+                    .Append(keyValue.Key)
+                    .Append("=\"")
+                    .Append(htmlHelper.Encode(keyValue.Value))
+                    .Append('"');
+            }
+
+            return new MvcHtmlString(sb.ToString());
         }
     }
 }
