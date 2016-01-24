8a33b2d1be8efed28a2349a1fcdec6e8b9478f82
Tim <tduncan72@gmail.com>
2016-01-24 10:42:47 -0500

    Remove fluent html from Confirm Email and change to use new modal

--- CritterHeroes.Web/Areas/Account/Models/ConfirmEmailModel.cs
+++ CritterHeroes.Web/Areas/Account/Models/ConfirmEmailModel.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using TOTD.Mvc.FluentHtml.Attributes;
 
 namespace CritterHeroes.Web.Areas.Account.Models
 {
     public class ConfirmEmailModel
     {
-        [Placeholder("Email")]
-        [DataType(DataType.EmailAddress)]
         public string Email
         {
             get;
             set;
         }
 
-        [Placeholder("Confirmation code")]
         public string ConfirmationCode
         {
             get;
