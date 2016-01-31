c95fb3c0294e9b7c7235ca4ef35755c11969dadf
Tim <tduncan72@gmail.com>
2016-01-31 12:40:27 -0500

    Move logic for creating azure logger based on serilog to base class

--- CritterHeroes.Web/DataProviders/Azure/Storage/Logging/AzureUserLogger.cs
+++ CritterHeroes.Web/DataProviders/Azure/Storage/Logging/AzureUserLogger.cs
@@ -1,54 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using CritterHeroes.Web.Contracts.Logging;
 using CritterHeroes.Web.Contracts.Storage;
-using CritterHeroes.Web.DataProviders.Azure.Utility;
-using CritterHeroes.Web.Models.Logging;
 using Microsoft.Owin;
-using Microsoft.WindowsAzure.Storage.Table;
-using Newtonsoft.Json;
 using Serilog;
 
 namespace CritterHeroes.Web.DataProviders.Azure.Storage.Logging
 {
-    public class AzureUserLogger : IUserLogger
+    public class AzureUserLogger : BaseAzureLogger, IUserLogger
     {
         private IOwinContext _owinContext;
-        private IAzureService _azureService;
-
-        private ILogger _logger;
 
         public AzureUserLogger(IAzureService azureService, IOwinContext owinContext)
+            : base(azureService)
         {
             this._owinContext = owinContext;
-            this._azureService = azureService;
-
-            _logger = Log.Logger = new LoggerConfiguration()
-                .WriteTo
-                .AzureTableStorage(azureService, "userlog")
-                .Enrich.WithProperty("IPAddress", _owinContext.Request.RemoteIpAddress)
-                .MinimumLevel.Information()
-                .CreateLogger();
         }
 
         public void LogAction(string message, params object[] messageValues)
         {
-            _logger.Information(message, messageValues);
+            Logger.Information(message, messageValues);
         }
 
         public void LogError(string message, params object[] messageValues)
         {
-            _logger.Error(message, messageValues);
+            Logger.Error(message, messageValues);
         }
 
         public void LogError(string message, IEnumerable<string> errors, params object[] messageValues)
         {
-            _logger
+            Logger
                 .ForContext("Errors", errors)
                 .Error(message, messageValues);
+        }
+
+        protected override LoggerConfiguration ConfigureLogger(LoggerConfiguration loggerConfiguration)
+        {
+            return base.ConfigureLogger(loggerConfiguration).Enrich.WithProperty("IPAddress", _owinContext.Request.RemoteIpAddress);
         }
     }
 }
---
+++ CritterHeroes.Web/DataProviders/Azure/Storage/Logging/BaseAzureLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Contracts.Storage;
+using Serilog;
+
+namespace CritterHeroes.Web.DataProviders.Azure.Storage.Logging
+{
+    public abstract class BaseAzureLogger
+    {
+        private readonly IAzureService _azureService;
+        private readonly string _tableName;
+
+        private ILogger _logger;
+
+        protected BaseAzureLogger(IAzureService azureService)
+            : this(azureService, "logs")
+        {
+        }
+
+        protected BaseAzureLogger(IAzureService azureService, string tableName)
+        {
+            this._azureService = azureService;
+            this._tableName = tableName;
+        }
+
+        protected ILogger Logger
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    _logger = CreateLogger();
+                }
+                return _logger;
+            }
+        }
+
+        protected virtual LoggerConfiguration ConfigureLogger(LoggerConfiguration loggerConfiguration)
+        {
+            return loggerConfiguration.WriteTo.AzureTableStorage(_azureService, _tableName);
+        }
+
+        private ILogger CreateLogger()
+        {
+            LoggerConfiguration configuration = ConfigureLogger(new LoggerConfiguration());
+            return configuration.MinimumLevel.Information().CreateLogger();
+        }
+    }
+}
