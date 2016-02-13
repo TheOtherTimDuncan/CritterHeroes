eaa7cba3cd762460acc255f3db491fdb39d501b2
Tim <tduncan72@gmail.com>
2016-02-13 10:30:10 -0500

    Add log sink to add messages to a list

---
+++ CH.Test/ListSinkTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CritterHeroes.Web.Common;
+using Serilog;
+
+namespace CH.Test
+{
+    [TestClass]
+    public class ListSinkTests
+    {
+        [TestMethod]
+        public void AddsLogEventToList()
+        {
+            List<string> messages = new List<string>();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .List(messages)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            Log.Debug("This is a {Test}", "test");
+
+            messages.Should().HaveCount(1);
+            messages.Single().Should().Be("This is a \"test\"");
+        }
+    }
+}
