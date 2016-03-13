07f42b18ec7845f391ab144927b6c8c919d8601d
Tim <tduncan72@gmail.com>
2016-03-13 14:08:03 -0400

    Change configuratons to not use symlinks

---
+++ Configurations/CopyConfigurations.bat
@@ -0,0 +1,7 @@
+COPY "%~dp0\AppSettings.config" "%~dp0\..\CritterHeroes.Web\AppSettings.config"
+COPY "%~dp0\AppSettings.config" "%~dp0\..\CH.Test\AppSettings.config"
+COPY "%~dp0\AppSettings.Migrations.config" "%~dp0\..\CH.DatabaseMigrator\AppSettings.config"
+
+COPY "%~dp0\RescueGroups.config" "%~dp0\..\CritterHeroes.Web\RescueGroups.config"
+COPY "%~dp0\RescueGroups.config" "%~dp0\..\CH.RescueGroupsExplorer\RescueGroups.config"
+COPY "%~dp0\RescueGroups.config" "%~dp0\..\CH.Test\RescueGroups.config"
---
+++ Configurations/CopyConnectionStrings.bat
@@ -0,0 +1,3 @@
+COPY "%~dp0\ConnectionStrings.Dev.config" "%~dp0\..\CH.Test\ConnectionStrings.config"
+COPY "%~dp0\ConnectionStrings.Dev.config" "%~dp0\..\CritterHeroes.Web\ConnectionStrings.config"
+COPY "%~dp0\ConnectionStrings.Migrator.config" "%~dp0\..\CH.DatabaseMigrator\ConnectionStrings.config"
