2cccb830c43d21ea2f8be937adaaa9119512b2de
Tim <tduncan72@gmail.com>
2016-03-15 19:27:50 -0400

    Add starting javascript module to act as a jQuery replacement

---
+++ CritterHeroes.Web/src/js/totd.dom.js
@@ -0,0 +1,28 @@
+﻿(function (totd, global) {
+
+    'use strict';
+
+    var query = function (selector) {
+        return new query.fn.init(selector);
+    };
+
+    query.fn = query.prototype = {
+
+        elements: [],
+        selector: '',
+        length: 0
+
+    };
+
+    query.fn.init = function (selector) {
+        this.elements = global.querySelectorAll(selector);
+        this.selector = selector;
+        this.length = this.elements.length;
+        return this;
+    };
+
+    totd.dom = {
+        query: query
+    };
+
+}(this.totd = this.totd || {}, document));
