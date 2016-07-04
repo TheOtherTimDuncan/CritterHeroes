039ff3de6f055f77584b3128e41ea6310be5ce93
Tim <tduncan72@gmail.com>
2016-07-04 18:41:10 -0400

    Add AgeHelper

---
+++ CritterHeroes.Web/Shared/AgeHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CritterHeroes.Web.Shared
+{
+    public static class AgeHelper
+    {
+        public static string GetLifeStage(string speciesName, DateTime? birthDate, string generalAge)
+        {
+            if (birthDate == null)
+            {
+                return generalAge;
+            }
+
+            int ageMonths = (DateTime.Now.Month - birthDate.Value.Month) + 12 * (DateTime.Now.Year - birthDate.Value.Year);
+
+            switch (speciesName)
+            {
+                case "Cat":
+                    if (ageMonths <= 6)
+                    {
+                        return "Kitten";
+                    }
+                    else if (ageMonths <= 24)
+                    {
+                        return "Young";
+                    }
+                    else if (ageMonths < 120)
+                    {
+                        return "Adult";
+                    }
+                    return "Senior";
+
+                case "Dog":
+                    if (ageMonths <= 6)
+                    {
+                        return "Puppy";
+                    }
+                    else if (ageMonths <= 18)
+                    {
+                        return "Young";
+                    }
+                    else if (ageMonths < 72)
+                    {
+                        return "Adult";
+                    }
+                    return "Senior";
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(speciesName), speciesName);
+            }
+        }
+    }
+}
