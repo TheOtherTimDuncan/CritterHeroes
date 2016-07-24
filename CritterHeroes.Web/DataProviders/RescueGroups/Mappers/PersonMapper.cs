93e91f8dce05ac5526977451de785a015b1e7d26
Tim <tduncan72@gmail.com>
2016-07-24 15:22:05 -0400

    Add BaseContactMapper

--- CritterHeroes.Web/DataProviders/RescueGroups/Mappers/PersonMapper.cs
+++ CritterHeroes.Web/DataProviders/RescueGroups/Mappers/PersonMapper.cs
@@ -10,69 +10,21 @@ using TOTD.Utility.StringHelpers;
 
 namespace CritterHeroes.Web.DataProviders.RescueGroups.Mappers
 {
-    public class PersonMapperContext : MapperContext<PersonSource, Person>
+    public class PersonMapperContext : BaseContactMapperContext<PersonSource, Person>
     {
         public PersonMapperContext(PersonSource source, Person target, IAppEventPublisher publisher)
             : base(source, target, publisher)
         {
         }
-
-        public IEnumerable<Group> Groups
-        {
-            get;
-            set;
-        }
-
-        public IEnumerable<PhoneType> PhoneTypes
-        {
-            get;
-            set;
-        }
-
-        public PhoneType PhoneTypeWork
-        {
-            get
-            {
-                return PhoneTypes.Single(x => x.Name.SafeEquals(PhoneTypeNames.Work));
-            }
-        }
-
-        public PhoneType PhoneTypeHome
-        {
-            get
-            {
-                return PhoneTypes.Single(x => x.Name.SafeEquals(PhoneTypeNames.Home));
-            }
-        }
-
-        public PhoneType PhoneTypeCell
-        {
-            get
-            {
-                return PhoneTypes.Single(x => x.Name.SafeEquals(PhoneTypeNames.Cell));
-            }
-        }
-
-        public PhoneType PhoneTypeFax
-        {
-            get
-            {
-                return PhoneTypes.Single(x => x.Name.SafeEquals(PhoneTypeNames.Fax));
-            }
-        }
     }
 
-    public class PersonMapper : Mapper<PersonSource, Person, PersonMapperContext>
+    public class PersonMapper : BaseContactMapper<PersonSource, Person, PersonMapperContext>
     {
         protected override void CreateConfiguration(MapperConfiguration<PersonSource, Person, PersonMapperContext> configuration)
         {
+            base.CreateConfiguration(configuration);
+
             configuration
-                .ForField
-                (
-                    fieldName: "contactID",
-                    fromAction: context => context.Source.ID = context.Target.RescueGroupsID.Value,
-                    toAction: context => context.Target.RescueGroupsID = context.Source.ID
-                )
                 .ForField
                 (
                     fieldName: "contactFirstName",
@@ -99,29 +51,6 @@ namespace CritterHeroes.Web.DataProviders.RescueGroups.Mappers
                 )
                 .ForField
                 (
-                    fieldName: "contactAddress",
-                    fromAction: context => context.Source.Address = context.Target.Address,
-                    toAction: context => context.Target.Address = context.Source.Address
-                )
-                .ForField
-                (
-                    fieldName: "contactCity",
-                    fromAction: context => context.Source.City = context.Target.City,
-                    toAction: context => context.Target.City = context.Source.City
-                )
-                .ForField
-                (
-                    fieldName: "contactState",
-                    fromAction: context => context.Source.State = context.Target.State,
-                    toAction: context => context.Target.State = context.Source.State
-                )
-                .ForField
-                (
-                    fieldName: "contactPostalcode",
-                    fromAction: context => context.Source.Zip = context.Target.Zip,
-                    toAction: context => context.Target.Zip = context.Source.Zip
-                ).ForField
-                (
                     fieldName: "contactPhoneWork",
                     fromAction: context => SetPhoneNumber(context.PhoneTypeWork, context.Target, (phone, ext) =>
                     {
@@ -129,22 +58,26 @@ namespace CritterHeroes.Web.DataProviders.RescueGroups.Mappers
                         context.Source.PhoneWorkExtension = ext;
                     }),
                     toAction: context => ImportPhoneNumber(context.Target, context.PhoneTypeWork, context.Source.PhoneWork, context.Source.PhoneWorkExtension)
-                ).ForField
+                )
+                .ForField
                 (
                     fieldName: "contactPhoneHome",
                     fromAction: context => SetPhoneNumber(context.PhoneTypeHome, context.Target, (phone, ext) => context.Source.PhoneHome = phone),
                     toAction: context => ImportPhoneNumber(context.Target, context.PhoneTypeHome, context.Source.PhoneHome)
-                ).ForField
+                )
+                .ForField
                 (
                     fieldName: "contactPhoneCell",
                     fromAction: context => SetPhoneNumber(context.PhoneTypeCell, context.Target, (phone, ext) => context.Source.PhoneCell = phone),
                     toAction: context => ImportPhoneNumber(context.Target, context.PhoneTypeCell, context.Source.PhoneCell)
-                ).ForField
+                )
+                .ForField
                 (
                     fieldName: "contactFax",
                     fromAction: context => SetPhoneNumber(context.PhoneTypeFax, context.Target, (phone, ext) => context.Source.PhoneFax = phone),
                     toAction: context => ImportPhoneNumber(context.Target, context.PhoneTypeFax, context.Source.PhoneFax)
-                ).ForField
+                )
+                .ForField
                 (
                     fieldName: "contactGroups",
                     toAction: context => ImportGroups(context.Target, context.Source.GroupNames, context.Groups)
