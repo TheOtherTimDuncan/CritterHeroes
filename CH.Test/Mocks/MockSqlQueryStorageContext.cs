2c30239044c36feeb530f96aa9327d16068488f5
Tim <tduncan72@gmail.com>
2016-07-28 19:35:34 -0400

    Add mock implementation of ISqlQueryStorageContext<>

---
+++ CH.Test/Mocks/MockSqlQueryStorageContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using CritterHeroes.Web.Contracts.Storage;
+using EntityFramework.Testing.Moq;
+using Moq;
+
+namespace CH.Test.Mocks
+{
+    public class MockSqlQueryStorageContext<T> : Mock<ISqlQueryStorageContext<T>> where T : class
+    {
+        private MockDbSet<T> _dbset;
+
+        public MockSqlQueryStorageContext()
+        {
+            _dbset = new MockDbSet<T>().SetupLinq();
+
+            _dbset.Setup(x => x.AsNoTracking()).Returns(_dbset.Object);
+
+            this.Setup(x => x.Entities).Returns(_dbset.Object);
+            this.Setup(x => x.GetAll()).Returns(() => _dbset.Object.ToList());
+            this.Setup(x => x.GetAllAsync()).Returns(async () => await _dbset.Object.ToListAsync());
+        }
+    }
+}
