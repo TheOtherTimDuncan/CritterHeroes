8c62e95693afcd8bd6d4aa4dcf7ac85907c4c654
Tim <tduncan72@gmail.com>
2016-07-28 19:30:49 -0400

    Add interface and concrete implementation for query-only storage context

---
+++ CritterHeroes.Web/Data/Contexts/SqlQueryStorageContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using CritterHeroes.Web.Contracts.Events;
+using CritterHeroes.Web.Contracts.Storage;
+
+namespace CritterHeroes.Web.Data.Contexts
+{
+    public class SqlQueryStorageContext<T> : BaseDbContext<SqlStorageContext<T>>, ISqlQueryStorageContext<T> where T : class
+    {
+        public SqlQueryStorageContext(IAppEventPublisher publisher)
+            : base(publisher)
+        {
+            Database.SetInitializer<SqlQueryStorageContext<T>>(null);
+        }
+
+        public virtual IDbSet<T> _Entities
+        {
+            get;
+            set;
+        }
+
+        public IQueryable<T> Entities
+        {
+            get
+            {
+                return _Entities.AsNoTracking();
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Entities.ToList();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await Entities.ToListAsync();
+        }
+    }
+}
