fc80012544aa3f4bbd51a2432692f52d0fb871b5
Tim <tduncan72@gmail.com>
2017-01-14 14:48:59 -0500

    Add email builder for email confirmation email

---
+++ CH.Test/EmailTests/ConfirmEmailEmailBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Shared.Commands;
+using CritterHeroes.Web.Shared.Email;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Mailer.Core;
+
+namespace CH.Test.EmailTests
+{
+    [TestClass]
+    public class ConfirmEmailEmailBuilderTests : EmailBuilderBaseTest
+    {
+        [TestMethod]
+        public void GeneratesCorrectHtml()
+        {
+            ConfirmEmailEmailCommand command = CreateTestCommand<ConfirmEmailEmailCommand, ConfirmEmailEmailCommand.ConfirmEmailData>();
+            command.EmailData.UrlConfirm = "urlconfirm";
+            command.EmailData.Token = "token";
+            command.EmailData.TokenLifespan = TimeSpan.FromHours(4);
+
+            ConfirmEmailEmailBuilder builder = new ConfirmEmailEmailBuilder();
+            EmailMessage message = builder.BuildEmail(command);
+
+            WriteEmailMessage(message, "ConfirmEmail");
+        }
+    }
+}
---
+++ CritterHeroes.Web/Shared/Email/ConfirmEmailEmailBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Shared.Commands;
+using TOTD.Mailer.Core;
+
+namespace CritterHeroes.Web.Shared.Email
+{
+    public class ConfirmEmailEmailBuilder : EmailBuilderBase<ConfirmEmailEmailCommand, ConfirmEmailEmailCommand.ConfirmEmailData>
+    {
+        protected override EmailBuilder BuildEmail(EmailBuilder builder, ConfirmEmailEmailCommand command)
+        {
+            return builder
+                .AddParagraph($"Please click the link below to confirm your email address. This link will be valid for {command.EmailData.TokenLifespanDisplay}.")
+                .AddButton(command.EmailData.UrlConfirm, "Confirm Email")
+                .WithSubject($"Email Confirmation - {command.EmailData.OrganizationFullName}");
+        }
+    }
+}
