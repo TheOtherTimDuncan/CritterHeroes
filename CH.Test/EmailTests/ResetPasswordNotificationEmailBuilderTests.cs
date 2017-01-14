48078bd30e55878a27d322a801bc53b1588a5428
Tim <tduncan72@gmail.com>
2017-01-13 19:45:05 -0500

    Add email builder for Reset Password Notification

---
+++ CH.Test/EmailTests/ResetPasswordNotificationEmailBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Models.Emails;
+using CritterHeroes.Web.Shared.Commands;
+using CritterHeroes.Web.Shared.Email;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Mailer.Core;
+
+namespace CH.Test.EmailTests
+{
+    [TestClass ]
+    public class ResetPasswordNotificationEmailBuilderTests : EmailBuilderBaseTest
+    {
+        [TestMethod]
+        public void GeneratesCorrectHtml()
+        {
+            ResetPasswordNotificationEmailCommand command = CreateTestCommand<ResetPasswordNotificationEmailCommand, BaseEmailData>();
+
+            ResetPasswordNotificationEmailBuilder builder = new ResetPasswordNotificationEmailBuilder();
+            EmailMessage message = builder.BuildEmail(command);
+
+            WriteEmailMessage(message, "ResetPasswordNotification");
+        }
+    }
+}
---
+++ CritterHeroes.Web/Shared/Email/ResetPasswordNotificationEmailBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CritterHeroes.Web.Models.Emails;
+using CritterHeroes.Web.Shared.Commands;
+using TOTD.Mailer.Core;
+
+namespace CritterHeroes.Web.Shared.Email
+{
+    public class ResetPasswordNotificationEmailBuilder : EmailBuilderBase<ResetPasswordNotificationEmailCommand, BaseEmailData>
+    {
+        protected override EmailBuilder BuildEmail(EmailBuilder builder, ResetPasswordNotificationEmailCommand command)
+        {
+            return builder
+                .AddParagraph("This is a notification that your password has been successfuly reset.")
+                .WithSubject($"Admin Notification - {command.EmailData.OrganizationFullName}");
+        }
+    }
+}
